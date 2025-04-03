97c960d3a08de2a0397f4edd66bfa26a9e65fdb7
Mikuláš Šťastný <mikulas.stastny@bringauto.com>
2025-04-03 12:58:51 +0200

    Change package tracker url, update Package name

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,8 +1,8 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
-BA_PACKAGE_LIBRARY(nlohmann-json                        v3.10.5 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
-BA_PACKAGE_LIBRARY(fleet-protocol-cxx-helpers-static    v1.1.1)
-BA_PACKAGE_LIBRARY(fleet-protocol-interface             v2.0.0 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(nlohmann-json            v3.10.5 NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.1.1)
+BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
 
 IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
     BA_PACKAGE_LIBRARY(fleet-http-client-shared             v1.5.0)
