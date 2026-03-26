0289b804740fdb86be8ffee12886a5a86e0180bd
jiri-skuta <jiri.skuta@bringauto.com>
2026-03-26 15:32:55 +0100

    Bump dependencies and add missing aeron and async-function-execution

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,11 +1,13 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
 BA_PACKAGE_LIBRARY(nlohmann-json            v3.10.5 NO_DEBUG ON)
-BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.1.1)
+BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.2.0)
+BA_PACKAGE_LIBRARY(async-function-execution v1.0.0)
+BA_PACKAGE_LIBRARY(aeron                   v1.48.6)
 BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
 
 IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
-    BA_PACKAGE_LIBRARY(fleet-http-client-shared             v1.5.0)
+    BA_PACKAGE_LIBRARY(fleet-http-client-shared             v2.0.1)
     BA_PACKAGE_LIBRARY(boost         v1.86.0)
     BA_PACKAGE_LIBRARY(cpprestsdk    v2.10.20)
     BA_PACKAGE_LIBRARY(zlib                                 v1.2.11)
