e8e04022626e7e5921a43bd950eb3c7b3e77851e
jiri-skuta <jiri.skuta@bringauto.com>
2026-04-02 18:54:39 +0200

    bump fleet-http-client-shared to v2.0.2

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -7,7 +7,7 @@ BA_PACKAGE_LIBRARY(aeron                   v1.48.6)
 BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
 
 IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
-    BA_PACKAGE_LIBRARY(fleet-http-client-shared             v2.0.1)
+    BA_PACKAGE_LIBRARY(fleet-http-client-shared             v2.0.2)
     BA_PACKAGE_LIBRARY(boost         v1.86.0)
     BA_PACKAGE_LIBRARY(cpprestsdk    v2.10.20)
     BA_PACKAGE_LIBRARY(zlib                                 v1.2.11)
