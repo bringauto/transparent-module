cf8a5bd7a91f4dbf2e4ee884616539fc8241addd
jiri-skuta <jiri.skuta@bringauto.com>
2026-06-09 10:51:53 +0200

    bump nlohmann-json to v3.12.0 and zlib to v1.3.2, version to 1.0.5

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
-BA_PACKAGE_LIBRARY(nlohmann-json            v3.10.5 NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(nlohmann-json            v3.12.0 NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.2.0)
 BA_PACKAGE_LIBRARY(async-function-execution v1.0.0)
 BA_PACKAGE_LIBRARY(aeron                   v1.48.6)
@@ -10,6 +10,6 @@ IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
     BA_PACKAGE_LIBRARY(fleet-http-client-shared             v2.0.2)
     BA_PACKAGE_LIBRARY(boost         v1.86.0)
     BA_PACKAGE_LIBRARY(cpprestsdk    v2.10.20)
-    BA_PACKAGE_LIBRARY(zlib                                 v1.2.11)
+    BA_PACKAGE_LIBRARY(zlib                                 v1.3.2)
 
 ENDIF ()
