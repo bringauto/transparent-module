9f224e3ea2c3fc17390d9f499e379f200db90631
jiri-skuta <jiri.skuta@bringauto.com>
2026-06-10 08:02:59 +0200

    bump fleet-protocol-interface to v2.1.0

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,7 +4,7 @@ BA_PACKAGE_LIBRARY(nlohmann-json            v3.12.0 NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(fleet-protocol-cpp       v1.2.0)
 BA_PACKAGE_LIBRARY(async-function-execution v1.0.0)
 BA_PACKAGE_LIBRARY(aeron                   v1.48.6)
-BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.1.0 NO_DEBUG ON)
 
 IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
     BA_PACKAGE_LIBRARY(fleet-http-client-shared             v2.0.2)
